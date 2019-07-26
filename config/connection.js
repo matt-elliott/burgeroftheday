f886a3cb296715ef1da481082c6833e5a24b7b1f
Matt Elliott <mattelliottus@gmail.com>
2019-07-26 01:27:05 -0700

    added logic to check for jawsdb_url environemtn variable

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-let connection = async function() {
-  return await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB,
-    port: process.env.DB_PORT
-  });
+if(process.env.JAWSDB_URL) {
+  let connection = async function() {
+    return await mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  module.exports = connection;
+} else {
+  let connection = async function() {
+    return await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB,
+      port: process.env.DB_PORT
+    });
+  }
+  module.exports = connection;
 }
-
-module.exports = connection;
